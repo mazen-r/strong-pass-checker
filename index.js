a9195ba501efa23897180a18fe987ac5498af83b
Mazen Ramadan <mazenramadan00@gmail.com>
2022-09-27 13:24:54 +0200

    Add index.js file

---
+++ index.js
@@ -0,0 +1,9 @@
+const strongPassword = require('./utils/strongPassword');
+const mediumPassword = require('./utils/mediumPassowrd');
+const weakPassowrd = require('./utils/weakPassword');
+
+module.exports = {
+    strongPassword: strongPassword,
+    mediumPassword: mediumPassword,
+    weakPassowrd: weakPassowrd
+};
